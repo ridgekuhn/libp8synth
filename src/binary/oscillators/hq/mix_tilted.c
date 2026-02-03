8cedc148a483197f61a09cade122b5626c036490
ridgek <ridgekuhn@users.noreply.github.com>
2026-02-02 20:29:14 -0500

    ADD - hq/mix_tilted.c

---
+++ src/binary/oscillators/hq/mix_tilted.c
@@ -0,0 +1,76 @@
+// #include "./phasors/sample_tilted.c"
+
+/**
+ * Mix tilted sawtooth oscillator
+ *
+ * @param osc_state Mixer channel oscillator state
+ * @param chunk_buffer Mixer channel chunk buffer
+ * @param chunk_len Length of chunk_buffer in samples
+ */
+void mix_supersaw(int *osc_state, short *chunk_buffer, int chunk_len) {
+  /*
+   * Oscillator state
+   */
+  const _Bool osc_buzz = osc_state[21] != 0;
+  const int osc_detune = osc_state[20];
+  const int osc_detune_m1 = osc_detune == 2 ? 1 : 0;
+  const int osc_vol = osc_state[7];
+  const int osc_phase = osc_state[1];
+  const int osc_phase_detuned = osc_state[3];
+  const int osc_phase_inc = osc_state[2];
+  const int osc_detune_phase_inc = osc_state[4];
+  const int osc_detune_phase =
+      osc_phase_inc == osc_detune_phase_inc ? osc_phase : osc_phase_detuned;
+  const int osc_amplitude = (osc_vol * 3) / 2;
+
+  /*
+   * Buffer Constants
+   */
+  const int freq = (osc_phase_inc * 22050) >> 16;
+  const int detune_freq = (osc_detune_phase_inc * 22050) >> 16;
+
+  /*
+   * Populate buffer
+   */
+  int cur_phase = osc_phase;
+  int cur_detune_phase = osc_detune_phase;
+
+  for (int i = 0; i < chunk_len; i += 1) {
+    const int detune_phase = cur_detune_phase << osc_detune_m1;
+
+    double amplitude = 0;
+    double detune_amplitude = 0;
+
+    if (osc_buzz) {
+      amplitude = (double)sample_tilted(freq, cur_phase, 0x1000) / 0x10000 * 4;
+
+      detune_amplitude =
+          (double)sample_tilted(detune_freq, detune_phase & 0xffff, 0x1000) /
+          0x10000 * 4;
+    }
+
+    amplitude = (double)sample_tilted(freq, cur_phase, 0x2000) / 0x10000;
+
+    detune_amplitude =
+        (double)sample_tilted(detune_freq, detune_phase & 0xffff, 0x2000) /
+        0x10000;
+
+    /*
+     * Mix sample
+     */
+    amplitude *= 0x4000;
+    detune_amplitude *= 0x2000;
+
+    const double s_prefader = amplitude + detune_amplitude;
+    const double s = (s_prefader * osc_amplitude) / 3072;
+    chunk_buffer[i] = (short)s;
+
+    // Increment phase
+    cur_phase = (cur_phase + osc_phase_inc) & 0xffff;
+    cur_detune_phase = (cur_detune_phase + osc_detune_phase_inc) & 0x1ffff;
+  }
+
+  // Update oscillator state
+  osc_state[1] = cur_phase;
+  osc_state[3] = cur_detune_phase;
+}
