2988179d8dc5b9f787d3d9ab23692593d5919b43
ridgek <ridgekuhn@users.noreply.github.com>
2026-03-03 17:12:18 -0500

    FIXUP - Use detune_amplitude in hq/mix_sin

--- src/binary/synth/oscillators/hq/mix_sin.c
+++ src/binary/synth/oscillators/hq/mix_sin.c
@@ -11,14 +11,14 @@ void mix_sin(int *osc_state, short *tick_buffer, int chunk_len) {
   /*
    * Oscillator state
    */
-  const _Bool osc_buzz = osc_state[21] != 0;
+  // const _Bool osc_buzz = osc_state[21] != 0;
   const int osc_phase = osc_state[1];
   const int osc_phase_detuned = osc_state[3];
   const int osc_phase_inc = osc_state[2];
   const int osc_vol = osc_state[7];
   const int osc_detune_phase_inc = osc_state[4];
-  const int osc_detune = osc_state[20];
-  const int osc_detune_m1 = osc_detune == 2 ? 1 : 0;
+  // const int osc_detune = osc_state[20];
+  // const int osc_detune_m1 = osc_detune == 2 ? 1 : 0;
   const int osc_detune_phase =
       osc_phase_inc == osc_detune_phase_inc ? osc_phase : osc_phase_detuned;
   const int osc_amplitude = (osc_vol * 3) / 2;
@@ -31,11 +31,14 @@ void mix_sin(int *osc_state, short *tick_buffer, int chunk_len) {
 
   for (int i = 0; i < chunk_len; i += 1) {
     const double radians = (((double)cur_phase / 0x10000) * M_PI * 2);
-    const double amplitude = sin(radians) * 0xc000;
+    double amplitude = sin(radians);
 
     const double detune_radians =
         (((double)(cur_detune_phase & 0xffff) / 0x10000) * M_PI * 2);
-    const double detune_amplitude = sin(radians) * 0xc000;
+    double detune_amplitude = sin(detune_radians);
+
+    amplitude *= 0x6000;
+    detune_amplitude *= 0x6000;
 
     const int s_pregain = amplitude / 4 + detune_amplitude / 8;
     const int s = (s_pregain * osc_amplitude) / 3072;
