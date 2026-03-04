a529fd5537583965029d66869ada41d5c8bcfe21
ridgek <ridgekuhn@users.noreply.github.com>
2026-03-04 18:52:52 -0500

    UPDATE - Add codo_state to binary/globals

--- src/binary/globals.c
+++ src/binary/globals.c
@@ -1,4 +1,16 @@
 #include "./globals.h"
 
+CodoState codo_state = {
+  .is_paused = 0,
+  .audio_playing = 0,
+  .codo_audio_buffer_init = 0,
+  .patterns_played = 0,
+  .cur_pat_idx = 0,
+  .prog_sample_rate = 22050,
+  .prog_channels = 1,
+  .sdl_sample_rate = 0,
+  .sdl_channels = 0,
+};
+
 int m_high = 0x1234567;
 int m_low = 0xdeadbeef;
--- src/binary/globals.h
+++ src/binary/globals.h
@@ -1,6 +1,46 @@
+#include <stdint.h>
+
 #ifndef GLOBALS
 #define GLOBALS
 
+/*
+ * Codo state
+ */
+struct CodoState {
+  // +0
+  int32_t UNKNOWN_0[0x97];
+  // +604
+  int32_t is_paused;
+  // +608
+  int32_t UNKNOWN_1;
+  // +612
+  int32_t audio_playing;
+  // +616
+  int32_t UNKNOWN_616[5];
+  // +632
+  int32_t codo_audio_buffer_init;
+  // +636
+  int32_t patterns_played;
+  // +640
+  int32_t cur_pat_idx;
+  // +644
+  int32_t UNKNOWN_644[4];
+  // +660
+  int32_t prog_sample_rate;
+  // +664
+  int32_t prog_channels;
+  // +668
+  int32_t sdl_sample_rate;
+  // +672
+  int32_t sdl_channels;
+};
+
+typedef struct CodoState CodoState;
+
+/*
+ * Variables
+ */
+extern CodoState codo_state;
 extern int m_low;
 extern int m_high;
 
