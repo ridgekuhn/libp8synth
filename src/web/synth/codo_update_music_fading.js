a3db1fa8a66b57246e364215bed14c3f9fbce0d1
ridgek <ridgekuhn@users.noreply.github.com>
2026-03-04 20:28:48 -0500

    FIXUP - Prevent music not starting in web/synth/codo_update_music_fading

--- src/web/synth/codo_update_music_fading.js
+++ src/web/synth/codo_update_music_fading.js
@@ -4,41 +4,52 @@
 function codo_update_music_fading() {
 	codo_lock_audio_plat();
 
-	let new_fade_vol = c[fade_vol];
-
-	if (c[fade_vol] != c[fade1] && c[fade_len] != 0) {
-		const time = codo_get_time();
-		const fade_progress = time - c[fade_start_t];
-
-		new_fade_vol = c[fade1];
-
-		if (c[fade_len] > fade_progress) {
-			new_fade_vol =
-				(B(c[fade1], fade_progress) +
-					B(c[fade0], c[fade_len] - fade_progress)) /
-				c[fade_len];
-
-			new_fade_vol = new_fade_vol < 0x10000 ? new_fade_vol : 0x10000;
-			new_fade_vol = new_fade_vol > 0 ? new_fade_vol : 0;
-
-			c[fade_vol] = new_fade_vol;
-
-			codo_unlock_audio_plat();
-
-			return;
+	if (c[fade_vol] == c[fade1]) {
+		if (c[fade_vol] == 0) {
+			c[MUSIC_PLAYING] = 0;
+			codo_stop_pico8_song(0);
 		}
+
+		codo_unlock_audio_plat();
+
+		return;
 	}
 
-	if (c[fade1] == 0) {
-		codo_lock_audio_plat();
+	if (c[fade_len] == 0) {
+		c[fade_vol] = c[fade1];
 
-		c[MUSIC_PLAYING] = 0;
+		if (c[fade1] == 0) {
+			c[MUSIC_PLAYING] = 0;
+			codo_stop_pico8_song(0);
+		}
 
-		codo_stop_pico8_song(0);
 		codo_unlock_audio_plat();
+
+		return;
+	}
+
+	const fade_progress = codo_get_time() - c[fade_start_t];
+
+	if (c[fade_len] > fade_progress) {
+		let new_fade_vol =
+			(B(c[fade1], fade_progress) + B(c[fade_0], c[fade_len] - fade_progress)) /
+			c[fade_len];
+		new_fade_vol = new_fade_vol < 65536 ? new_fade_vol : 65536;
+		new_fade_vol = new_fade_vol > 0 ? new_fade_vol : 0;
+
+		c[fade_vol] = new_fade_vol;
+
+		codo_unlock_audio_plat();
+
+		return;
+	}
+
+	c[fade_vol] = c[fade1];
+
+	if (c[fade1] == 0) {
+		c[MUSIC_PLAYING] = 0;
+		codo_stop_pico8_song(0);
 	}
 
 	codo_unlock_audio_plat();
-
-	return;
 }
