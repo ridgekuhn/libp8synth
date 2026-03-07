7231998889da0b6996dc55f2c3d3ff48b5a88410
ridgek <ridgekuhn@users.noreply.github.com>
2026-03-07 00:58:22 -0500

    ADD - binary/cart/codo_load_pico8_cart_from_file

---
+++ src/binary/cart/codo_load_pico8_cart_from_file.c
@@ -0,0 +1,45 @@
+// NOTICE: This file is not accurate and only partially implemented
+
+#include "./codo_load_pico8_cart_from_file.h"
+#include "../globals.h"
+#include "./cdata.h"
+#include "./codo_create_pico8_cart.h"
+#include "./load_music.h"
+#include "./verify_cart_header.h"
+#include <regex.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * Load Pico-8 Cart from File
+ */
+CData *codo_load_pico8_cart_from_file(FILE *file_ptr, int unknown) {
+  /*
+   * Verify file
+   */
+  if (verify_cart_header(file_ptr) != 0) {
+    return 0;
+  }
+
+  /*
+   * Populate CData
+   */
+  // Line buffer
+  char str[0x30001] = "";
+  // Regex for section tags
+  regex_t regex;
+  regcomp(&regex, "^__[a-z]*__\0?$", 0);
+  // Cart data struct
+  CData *cdata = codo_create_pico8_cart();
+
+  while (fgets(str, 0x30001, file_ptr)) {
+    // If current line is section tag
+    if (regexec(&regex, str, 0, NULL, 0) == 0) {
+      if (strstr(str, "__music__")) {
+        load_music(file_ptr, cdata);
+      }
+    }
+  }
+
+  return cdata;
+}
---
+++ src/binary/cart/codo_load_pico8_cart_from_file.h
@@ -0,0 +1,4 @@
+#include "./cdata.h"
+#include <stdio.h>
+
+CData *codo_load_pico8_cart_from_file(FILE *file_ptr, int offset);
